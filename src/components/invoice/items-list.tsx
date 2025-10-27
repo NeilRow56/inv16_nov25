b3eeaaeff57dea4a7e891ae4d7c45d184a5b4151
NeilRow56 <neilrowland56@gmail.com>
2025-10-27 06:35:14 +0000

    Added mapping function to items list component

--- src/components/invoice/items-list.tsx
+++ src/components/invoice/items-list.tsx
@@ -3,18 +3,40 @@ import { Button } from '../ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 import InvoiceItem from './invoice-item'
 
+// Dummy data so that we can set up the mapping functionality
+
+export const items = [
+  {
+    id: 1,
+    description: 'Website Design',
+    quantity: 1,
+    rate: 500,
+    amount: 500
+  },
+  {
+    id: 2,
+    description: 'Hosting (12 months)',
+    quantity: 1,
+    rate: 120,
+    amount: 120
+  }
+]
+
 export default function ItemsList() {
+  const addItem = () => {}
   return (
     <Card>
       <CardHeader className='flex flex-row items-center justify-between'>
         <CardTitle>Invoice Items</CardTitle>
-        <Button size='sm'>
+        <Button onClick={addItem} size='sm'>
           <Plus className='mr-2 h-4 w-4' />
           Add Item
         </Button>
       </CardHeader>
       <CardContent className='space-y-4'>
-        <InvoiceItem />
+        {items.map(item => (
+          <InvoiceItem key={item.id} />
+        ))}
       </CardContent>
     </Card>
   )
